9b82f0634cf925dfd5fbc366b4291e02fe7ce0f3
Santiago Ramírez <santiagoramirez@Student2s-MacBook-Pro.local>
2018-11-19 16:11:32 -0500

    anota los modelos con los nombres de los campos

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: currencies
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string(100)
+#  price_buy  :decimal(, )
+#  price_sale :decimal(, )
+#  available  :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#  country    :string
+#
+
 class Currency < ApplicationRecord
     belongs_to :user
 end
