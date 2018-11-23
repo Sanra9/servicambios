e8a45ce811adba80be17e8b6ea1f1fb4e88f538c
Santiago Ramírez <santiagoramirez@Student2s-MacBook-Pro.local>
2018-11-23 16:29:40 -0500

    Comprueba que database test funciona en rails c test

--- test/controllers/currencies_controller_test.rb
+++ test/controllers/currencies_controller_test.rb
@@ -4,24 +4,24 @@ class CurrenciesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  
+
   test 'cretae a currency.name' do
     assert_equal 'euro', currencies(:euro).name
   end
 
-  test 'create a currency.price_buy'
+  test 'create a currency.price_buy' do
     assert_equal 9.5, currencies(:euro).price_buy
   end
 
-  test 'create a currency.price_sale'
+  test 'create a currency.price_sale' do
     assert_equal BigDecimal.new("9.7"), currencies(:euro).price_sale
   end
 
-  test 'create a currency.available'
+  test 'create a currency.available' do
     assert_equal false, currencies(:euro).available
   end
 
-  test 'create a currency.country'
+  test 'create a currency.country' do
     assert_equal 'EU', currencies(:euro).country
   end
 end
