bbcfbcd5378d9de77d9fd8f9994352e0833a4b0c
Santiago Ramírez <santiagoramirez@Student2s-MacBook-Pro.local>
2018-11-23 16:22:29 -0500

    Modifica la table user, campo email-unique email:true, crea test para llenar campos de currency

--- test/controllers/currencies_controller_test.rb
+++ test/controllers/currencies_controller_test.rb
@@ -4,4 +4,24 @@ class CurrenciesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+  test 'cretae a currency.name' do
+    assert_equal 'euro', currencies(:euro).name
+  end
+
+  test 'create a currency.price_buy'
+    assert_equal 9.5, currencies(:euro).price_buy
+  end
+
+  test 'create a currency.price_sale'
+    assert_equal BigDecimal.new("9.7"), currencies(:euro).price_sale
+  end
+
+  test 'create a currency.available'
+    assert_equal false, currencies(:euro).available
+  end
+
+  test 'create a currency.country'
+    assert_equal 'EU', currencies(:euro).country
+  end
 end
